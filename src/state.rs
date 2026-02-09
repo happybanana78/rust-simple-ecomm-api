723249800d524c3d6adcdf523f3b6bf8fc4b9938
happybanana78 <leonardo.mazzoleni1@gmail.com>
2026-02-09 14:35:00 +0100

    refactored role module by introducing `RoleRepository` and `RoleService` structures, centralized dependency management in `AppState`, and updated methods for improved modularity and readability

--- src/state.rs
+++ src/state.rs
@@ -1,3 +1,4 @@
+use crate::auth::service::AuthService;
 use crate::cart::cart_items::service::CartItemsService;
 use crate::cart::guest_cart::service::GuestCartService;
 use crate::cart::user_cart::service::UserCartService;
@@ -6,6 +7,7 @@ use crate::users::service::UserService;
 use sqlx::PgPool;
 
 pub struct AppState {
+    pub auth_service: AuthService,
     pub product_service: ProductService,
     pub user_cart_service: UserCartService,
     pub guest_cart_service: GuestCartService,
@@ -16,6 +18,7 @@ pub struct AppState {
 impl AppState {
     pub fn new(pool: PgPool) -> Self {
         Self {
+            auth_service: AuthService::new(pool.clone()),
             product_service: ProductService::new(pool.clone()),
             user_cart_service: UserCartService::new(pool.clone()),
             guest_cart_service: GuestCartService::new(pool.clone()),
