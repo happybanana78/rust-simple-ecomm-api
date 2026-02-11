ac9648233db9f30961e6b63d872b53323154de0f
happybanana78 <leonardo.mazzoleni1@gmail.com>
2026-02-11 14:22:47 +0100

    refactored product tests to use `TestContext` for centralized setup logic, improving readability and reducing code duplication

--- tests/product_tests.rs
+++ tests/product_tests.rs
@@ -2,26 +2,20 @@ mod utils;
 
 #[actix_rt::test]
 async fn test_product_index() {
-    let test_db = utils::TestDatabase::new().await;
-    utils::seed_products(&test_db.pool).await;
+    let context = utils::TestContext::new(None).await;
 
-    let srv = utils::create_test_server(test_db.pool.clone());
-
-    let res = srv.get("/products/list").send().await.unwrap();
+    let res = context.srv.get("/products/list").send().await.unwrap();
     assert!(res.status().is_success());
 
-    test_db.cleanup().await;
+    context.database.cleanup().await;
 }
 
 #[actix_rt::test]
 async fn test_product_show() {
-    let test_db = utils::TestDatabase::new().await;
-    utils::seed_products(&test_db.pool).await;
+    let context = utils::TestContext::new(None).await;
 
-    let srv = utils::create_test_server(test_db.pool.clone());
-
-    let res = srv.get("/products/get/1").send().await.unwrap();
+    let res = context.srv.get("/products/get/1").send().await.unwrap();
     assert!(res.status().is_success());
 
-    test_db.cleanup().await;
+    context.database.cleanup().await;
 }
