1d8c513ca4616df405b44b3f768ba6cbfcea15d8
happybanana78 <leonardo.mazzoleni1@gmail.com>
2026-02-12 20:34:24 +0100

    added `seed_admin_user` function to seed admin user data, updated `.env` handling in seeding script, and switched to `DEV_DATABASE_URL` for development environment

--- src/bin/seed.rs
+++ src/bin/seed.rs
@@ -1,16 +1,16 @@
-use dotenvy::dotenv;
+use dotenvy::from_filename;
 use ecomm::db::seeders;
 use sqlx::PgPool;
 use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
-    dotenv().ok();
-    env_logger::init();
+    from_filename(".env.dev").ok();
+    env_logger::try_init().ok();
 
     println!("Starting seeding...");
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let database_url = env::var("DEV_DATABASE_URL").expect("DEV_DATABASE_URL must be set");
     let pool = PgPool::connect(&database_url)
         .await
         .expect("Failed to create pool");
--- src/db/seeders.rs
+++ src/db/seeders.rs
@@ -24,7 +24,32 @@ pub async fn seed_roles(pool: &PgPool) -> Result<(), sqlx::Error> {
     Ok(())
 }
 
+pub async fn seed_admin_user(pool: &PgPool) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        INSERT INTO users (id, username, email, password)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        1,
+        "admin",
+        "admin@admin.com",
+        "$argon2id$v=19$m=19456,t=2,p=1$cvMtKFBV7DMHHq7DLLCDAg$JbZ3kqb47wjU5IxeZmeea/6yYIC8Yz6Xqe1KwgWwroc"
+    )
+        .execute(pool)
+        .await?;
+
+    sqlx::query!(
+        "INSERT INTO user_has_roles (user_id, role_id) VALUES (1, 1)
+         ON CONFLICT DO NOTHING;"
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(())
+}
+
 pub async fn seed_all(pool: &PgPool) -> Result<(), sqlx::Error> {
     seed_roles(pool).await?;
+    seed_admin_user(pool).await?;
     Ok(())
 }
