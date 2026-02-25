644a52fe16d387d7565dd60be19f849c63c16fb8
happybanana78 <leonardo.mazzoleni1@gmail.com>
2026-02-25 11:06:21 +0100

    added `validate_target_index` function to ensure optional index is present, non-negative, and converted to `usize`, updated `validation_utils` with `HashMap` for improved error handling

--- src/validation_utils.rs
+++ src/validation_utils.rs
@@ -1,4 +1,5 @@
 use crate::errors::error::AppError;
+use std::collections::HashMap;
 
 pub fn validate_slug(slug: &str) -> Result<(), AppError> {
     if slug.contains(" ") {
@@ -8,4 +9,30 @@ pub fn validate_slug(slug: &str) -> Result<(), AppError> {
     } else {
         Ok(())
     }
+}
+
+pub fn validate_target_index(target_index: Option<i32>) -> Result<usize, AppError> {
+    let target_index = target_index.ok_or_else(|| {
+        let mut error = HashMap::new();
+
+        error.insert(
+            "target_index".to_string(),
+            vec!["field is required".to_string()],
+        );
+
+        AppError::ValidationSingle(error)
+    })?;
+
+    if target_index < 0 {
+        let mut error = HashMap::new();
+
+        error.insert(
+            "target_index".to_string(),
+            vec!["must be at least 0".to_string()],
+        );
+
+        return Err(AppError::ValidationSingle(error));
+    }
+
+    Ok(target_index as usize)
 }
